af01f9256ebf2f3b82867f1f6b6d653c21172bcf
RPF511 <sbk98726@gmail.com>
2023-04-07 23:15:04 +0900

    create react app

---
+++ src/Components/App.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect }  from "react";
+
+const App = () => {
+   
+  
+    return (
+      <div>
+        
+        <h1>hello world</h1>
+  
+      </div>
+    );
+  };
+  
+  export default App;
